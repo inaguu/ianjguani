0d377732e7525a5f45136e0d827044cd8a80b9e8
inaguu <ianjguani@gmail.com>
2024-07-09 13:53:34 -0700

    small change to skills page

--- src/pages/SkillsPage.jsx
+++ src/pages/SkillsPage.jsx
@@ -1,10 +1,9 @@
 
 function SkillsPage() {
     return (
-        <div id='content' className=''>
-            <div className=''>
-                skills
-            </div>
+        <div class="container relative flex flex-col justify-start h-[91%] max-w-6xl px-10 mx-auto xl:px-0 mt-5">
+            <h2 class="mb-1 text-3xl font-extrabold leading-tight text-gray-900">Skills</h2>
+            <p class="mb-12 text-lg text-gray-500">My personal skills that I would bring to the table. </p>
         </div>
     );
 }
