d55af061ef50b3799906ec11e522cccc4bd28df0
inaguu <ianjguani@gmail.com>
2024-09-03 17:38:16 -0700

    design: removed navbar and edited projects page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,14 +16,14 @@ export default function Navbar() {
                         </svg>
                     </button>
                     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+                        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white mr-36">
                             <li>
                                 <a href="/skills" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" aria-current='page'>Skills</a>
                             </li>
                             <li>
                                 <a href="/projects" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Projects</a>
                             </li>
-                            <li>
+                            {/* <li>
                                 <a href="/education" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Education</a>
                             </li>
                             <li>
@@ -31,7 +31,7 @@ export default function Navbar() {
                             </li>
                             <li>
                                 <a href="/contact" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Contact</a>
-                            </li>
+                            </li> */}
                         </ul>
                     </div>
                 </div>
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -11,7 +11,7 @@ function ProjectPage() {
                             <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-indigo-500 rounded-lg"></span>
                             <div class="relative h-full p-5 bg-white border-2 border-indigo-500 rounded-lg">
                                 <div class="flex items-center -mt-1">
-                                    <h3 class="my-2 ml-3 mr-3 text-lg font-bold text-gray-800">Sushi Maker</h3>
+                                    <h3 class="my-2 ml-3 mr-3 text-lg font-bold text-gray-800">MRAD</h3>
                                     <a href="https://github.com/">
                                         <svg className='inline-block h-[20px] w-[20px]' viewBox='0 0 16 16' fill='var(--accent-text)'>
                                             <path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z'></path>
@@ -21,12 +21,12 @@ function ProjectPage() {
                                 <div class="flex items-center -mt-1">
                                     <h6 className="my-2 ml-3 font-bold text-gray-800">Developer</h6>
                                     <h6 className="my-2 ml-1 font-bold">|</h6>
-                                    <h6 className="my-2 ml-1 font-bold text-gray-600">Imgur meets Pastebin meets Bit.ly</h6>
+                                    <h6 className="my-2 ml-1 font-bold text-gray-600">Student Randomizer for Hospitals</h6>
                                 </div>
 
                                 <p class="mt-3 mb-1 text-xs font-medium text-indigo-500 uppercase">------------</p>
-                                <p class="mb-2 text-gray-600">A simple web application that utilizes the features of three separate well-known websites. It combines the
-                                    websites called Imgur, Bit.ly, and Pastebin to simpify the needs of users who frequently use all three.</p>
+                                <p class="mb-2 text-gray-600">A simple web application for BCIT's Medical Radiography program to randomly assign students to hospitals
+                                    for their practicum semesters. It uses EJs, Javascript, and CSS which are the basic languages when making a website.</p>
                             </div>
                         </div>
                     </div>
@@ -35,7 +35,7 @@ function ProjectPage() {
                             <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-purple-500 rounded-lg"></span>
                             <div class="relative h-full p-5 bg-white border-2 border-purple-500 rounded-lg">
                                 <div class="flex items-center -mt-1">
-                                    <h3 class="my-2 ml-3 mr-3 text-lg font-bold text-gray-800">BC REPIT</h3>
+                                    <h3 class="my-2 ml-3 mr-3 text-lg font-bold text-gray-800">Personal Website</h3>
                                     <a href="https://github.com/">
                                         <svg className='inline-block h-[20px] w-[20px]' viewBox='0 0 16 16' fill='var(--accent-text)'>
                                             <path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z'></path>
@@ -45,13 +45,12 @@ function ProjectPage() {
                                 <div class="flex items-center -mt-1">
                                     <h6 className="my-2 ml-3 font-bold text-gray-800">Developer</h6>
                                     <h6 className="my-2 ml-1 font-bold">|</h6>
-                                    <h6 className="my-2 ml-1 font-bold text-gray-600">Damage Report System</h6>
+                                    <h6 className="my-2 ml-1 font-bold text-gray-600">Portfolio</h6>
                                 </div>
 
                                 <p class="mt-3 mb-1 text-xs font-medium text-purple-500 uppercase">------------</p>
-                                <p class="mb-2 text-gray-600">A damage report web application that centralizes BCIT's current damage report system. It's ease of
-                                    use allows new users to quickly grasp and report any damages. The admins are able to view all the reports and change
-                                    their statuses as they see fit.</p>
+                                <p class="mb-2 text-gray-600">A personal portfolio website to display my skills and projects. It is built on the React framework using JSX and
+                                    Tailwind to design the website itself.</p>
                             </div>
                         </div>
                     </div>
@@ -62,11 +61,19 @@ function ProjectPage() {
                             <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-blue-400 rounded-lg"></span>
                             <div class="relative h-full p-5 bg-white border-2 border-blue-400 rounded-lg">
                                 <div class="flex items-center -mt-1">
-                                    <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">Project Audit</h3>
+                                    <h3 class="my-2 ml-3 mr-3 text-lg font-bold text-gray-800">Sushi Maker</h3>
+                                    <a href="https://github.com/">
+                                        <svg className='inline-block h-[20px] w-[20px]' viewBox='0 0 16 16' fill='var(--accent-text)'>
+                                            <path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z'></path>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div class="flex items-center -mt-1">
+                                    <h6 className="my-2 ml-3 font-bold text-gray-800">Developer</h6>
                                 </div>
                                 <p class="mt-3 mb-1 text-xs font-medium text-blue-400 uppercase">------------</p>
-                                <p class="mb-2 text-gray-600">A Project Audit is a formal review of a project, which is intended
-                                    to assess the extent up to which project management standards are being upheld.</p>
+                                <p class="mb-2 text-gray-600">A simple web application that utilizes the features of three seperate well-known websites. It combines the websites called
+                                    Imgur, Bit.ly, and Pastebin to simplify the needs of users who frequently use all three. </p>
                             </div>
                         </div>
                     </div>
@@ -75,7 +82,15 @@ function ProjectPage() {
                             <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-yellow-400 rounded-lg"></span>
                             <div class="relative h-full p-5 bg-white border-2 border-yellow-400 rounded-lg">
                                 <div class="flex items-center -mt-1">
-                                    <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">Hacking / RE</h3>
+                                    <h3 class="my-2 ml-3 mr-3 text-lg font-bold text-gray-800">BC REPIT</h3>
+                                    <a href="https://github.com/">
+                                        <svg className='inline-block h-[20px] w-[20px]' viewBox='0 0 16 16' fill='var(--accent-text)'>
+                                            <path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z'></path>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div class="flex items-center -mt-1">
+                                    <h6 className="my-2 ml-3 font-bold text-gray-800">Developer</h6>
                                 </div>
                                 <p class="mt-3 mb-1 text-xs font-medium text-yellow-400 uppercase">------------</p>
                                 <p class="mb-2 text-gray-600">A security hacker is someone who explores methods for breaching
@@ -88,7 +103,15 @@ function ProjectPage() {
                             <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-green-500 rounded-lg"></span>
                             <div class="relative h-full p-5 bg-white border-2 border-green-500 rounded-lg">
                                 <div class="flex items-center -mt-1">
-                                    <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">Bot/Script Development</h3>
+                                    <h3 class="my-2 ml-3 mr-3 text-lg font-bold text-gray-800">fix it</h3>
+                                    <a href="https://github.com/">
+                                        <svg className='inline-block h-[20px] w-[20px]' viewBox='0 0 16 16' fill='var(--accent-text)'>
+                                            <path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z'></path>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div class="flex items-center -mt-1">
+                                    <h6 className="my-2 ml-3 font-bold text-gray-800">Developer</h6>
                                 </div>
                                 <p class="mt-3 mb-1 text-xs font-medium text-green-500 uppercase">------------</p>
                                 <p class="mb-2 text-gray-600">Bot development frameworks were created as advanced software tools
