25b25aaa42a17e913cffa602e9cead95cf1b238b
inaguu <ianjguani@gmail.com>
2024-09-04 13:22:15 -0700

    bug: fixed redirecting for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+
+
 
 export default function Navbar() {
+    const navigate = useNavigate()
+
+    const goToHome = () => {
+        navigate('/')
+    }
+
+    const goToProjects = () => {
+        navigate('/projects')
+    }
+
+    const goToAbout = () => {
+        navigate('/skills')
+    }
+
     return (
         <div id='nav-menu' className=''>
             <nav class="bg-white border-gray-200 border-b-2">
                 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse ">
+                    <button onClick={goToHome} class="flex items-center space-x-3 rtl:space-x-reverse ">
                         <i class="h-8" alt="Flowbite Logo">&lt;/&gt;</i>
                         <span class="self-center text-2xl font-semibold whitespace-nowrap hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Ian Jeremy Guani</span>
-                    </a>
+                    </button>
                     <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -18,10 +35,10 @@ export default function Navbar() {
                     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                         <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white mr-36">
                             <li>
-                                <a href="/projects" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Projects</a>
+                                <button onClick={goToProjects} class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Projects</button>
                             </li>
                             <li>
-                                <a href="/skills" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About</a>
+                                <button onClick={goToAbout} class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About</button>
                             </li>
                             {/* <li>
                                 <a href="/contact" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Contact</a>
